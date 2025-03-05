8118937eebc50ffca2579da9873f882c522cade4
mingcheng <mingcheng@apache.org>
2025-03-05 11:29:05 +0800

    feat: implement signoff feature for commit messages ✍️
    - Add signoff option to CLI arguments in `cli.rs`
    - Implement author name and email retrieval in `git.rs`
    - Append signoff to commit message in `main.rs` if enabled
    - Add tests for author name and email functions in `git.rs`
    
    Signed-off-by: mingcheng <mingcheng@apache.org>

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@
  * File Created: 2025-03-01 17:17:30
  *
  * Modified By: mingcheng (mingcheng@apache.org)
- * Last Modified: 2025-07-11 18:39:26
+ * Last Modified: 2025-09-26 14:41:04
  */
 
 use aigitcommit::cli::Cli;
@@ -132,6 +132,19 @@ async fn main() -> std::result::Result<(), Box<dyn Error>> {
             return Err(message.into());
         }
     };
+
+    // If the --signoff option is enabled, add signoff to the commit message
+    if cli.signoff {
+        trace!("signoff option is enabled, will add signoff to the commit message");
+        let (author_name, author_email) = (
+            repository.get_author_name()?,
+            repository.get_author_email()?,
+        );
+
+        // Add signoff to the commit message
+        let signoff = format!("\n\nSigned-off-by: {} <{}>", author_name, author_email);
+        result.push_str(&signoff);
+    }
 
     // Detect auto signoff from environment variable
     let need_signoff = cli.signoff
