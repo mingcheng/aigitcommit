ab19afdee44c577d76e170190e3fe8cec5ebfa7d
mingcheng <mingcheng@apache.org>
2025-03-05 11:29:05 +0800

    feat: add auto signoff detection from environment variable ✍️
    - Implement detection of GIT_AUTO_SIGNOFF environment variable
    - Enable signoff if environment variable is set to true or 1
    - Update logic to combine CLI signoff option with environment variable check
    
    Signed-off-by: mingcheng <mingcheng@apache.org>

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@
  * File Created: 2025-03-01 17:17:30
  *
  * Modified By: mingcheng (mingcheng@apache.org)
- * Last Modified: 2025-07-11 17:39:40
+ * Last Modified: 2025-07-11 17:42:00
  */
 
 use aigitcommit::cli::Cli;
@@ -127,8 +127,13 @@ async fn main() -> std::result::Result<(), Box<dyn Error>> {
         }
     };
 
+    // Detect auto signoff from environment variable
+    let need_signoff_from_env = env::var("GIT_AUTO_SIGNOFF")
+        .map(|v| v == "1" || v.eq_ignore_ascii_case("true"))
+        .unwrap_or(false);
+
     // If the --signoff option is enabled, add signoff to the commit message
-    if cli.signoff {
+    if cli.signoff || need_signoff_from_env {
         trace!("signoff option is enabled, will add signoff to the commit message");
         let (author_name, author_email) = (
             repository.get_author_name()?,
