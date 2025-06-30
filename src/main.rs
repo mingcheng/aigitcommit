bdec9bbe2539f763c87c097b0ea513d08f42f7e2
明城 <mingcheng@apache.org>
2025-07-01 07:13:53 +0800

    docs: correct environment variable name in README 📚
    - Update `AIGITCOMMIT_SIGNOFF` to `GIT_AUTO_SIGNOFF` in README for consistency
    - Reflect the correct variable used in the code for auto signoff feature
    
    Signed-off-by: mingcheng <mingcheng@apache.org>

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@
  * File Created: 2025-03-01 17:17:30
  *
  * Modified By: mingcheng (mingcheng@apache.org)
- * Last Modified: 2025-09-26 14:42:21
+ * Last Modified: 2025-09-26 14:42:28
  */
 
 use aigitcommit::cli::Cli;
